cb6db92bfb36ca7a0c9d97db269459eee40b1922
Timothy Helton <timothy.helton@semanticbits.com>
2020-01-04 10:47:18 -0700

    MNT: update package build configuration files

--- setup.py
+++ setup.py
@@ -3,9 +3,52 @@
 
 from codecs import open
 from pathlib import Path
+from operator import itemgetter
 import re
+from typing import Iterable, List, Union
 
 from setuptools import setup, find_packages
+
+
+dependencies = {
+    'build': {
+        'setuptools',
+        'wheel',
+    },
+    'docs': {
+        'sphinx',
+        'sphinx_rtd_theme',
+    },
+    'jupyter': {
+        'jupyter',
+        'jupyterlab',
+    },
+    'profile': {
+        'memory_profiler',
+        'snakeviz',
+    },
+    'test': {
+        'Faker',
+        'git-lint',
+        'pytest',
+        'pytest-cov',
+        'pytest-pep8',
+    },
+}
+
+
+def combine_dependencies(extras: Union[str, Iterable[str]]) -> List[str]:
+    """
+    Combine package dependencies.
+
+    :param extras: key(s) from the `dependencies` dictionary
+    :return: The minimum set of package dependencies contained in `extras`.
+    """
+    if isinstance(extras, str):
+        deps = set(itemgetter(extras)(dependencies))
+    else:
+        deps = set().union(*itemgetter(*extras)(dependencies))
+    return list(deps)
 
 
 with open('tensorflow_2/__init__.py', 'r') as fd:
@@ -33,8 +76,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Build Tools',
-        ],
-    keywords='EnterKeywordsHere',
+    ],
+    keywords='TensorFlow, Training',
     packages=find_packages(exclude=[
         'data',
         'docker',
@@ -46,18 +89,18 @@ setup(
     ),
     install_requires=[
         'click',
-        ],
+        'matplotlib',
+        'numpy',
+        'plotly',
+    ],
     extras_require={
-        'build': ['setuptools', 'wheel'],
-        'data': ['cufflinks', 'matplotlib', 'pandas'],
-        'database': ['psycopg2', 'sqlalchemy'],
-        'docs': ['sphinx', 'sphinx_rtd_theme'],
-        'notebook': ['jupyter', 'jupyterlab'],
-        'profile': ['memory_profiler', 'snakeviz'],
-        'test': ['pytest', 'pytest-pep8'],
-        'tf-cpu': ['tensorflow'],
-        'tf-gpu': ['tensorflow-gpu'],
-        },
+        'all': combine_dependencies(dependencies.keys()),
+        'build': combine_dependencies(('build', 'test')),
+        'docs': combine_dependencies('docs'),
+        'jupyter': combine_dependencies('jupyter'),
+        'profile': combine_dependencies('profile'),
+        'test': combine_dependencies('test'),
+    },
     package_dir={'tensorflow_2': 'tensorflow_2'},
     include_package_data=True,
     entry_points={
